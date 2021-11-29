79ad845c1b5f7816e0b30af1c7f132ea9f31b999
Shradha-singh23 <shradha23singh@gmail.com>
2021-11-30 01:44:49 +0530

    Navigation in list using keyboard buttons

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import data from "../data";
 import './Searchbar.css';
 
 const Searchbar = () => {
+    const [isVisible, setIsVisible] = useState(false);
     const [filteredArray, setFilteredArray] = useState([]);
-    const [hasFocus, setHasFocus] = useState(false);
+    const [cursor, setCursor] = useState(-1);
+
+    const searchContainer = useRef(null);
+    const searchResultRef = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            window.removeEventListener("mousedown", handleClickOutside)
+        }
+    },[])
+
+    const showSuggestion = () => setIsVisible(true);
+
+    const hideSuggestion = () => setIsVisible(false);
+
+    const handleClickOutside = (event) => {
+        if(searchContainer.current && !searchContainer.current.contains(event.target)){
+            hideSuggestion()
+        };
+    };
 
     const handleOnChange = (e) => {
         const searchedWord = e.target.value;
@@ -14,41 +36,40 @@ const Searchbar = () => {
         setFilteredArray(newArray)
     }
 
-    const handleOnFocus = () => {
-        setHasFocus(true)
-    }
-
-    const handleOnBlur = () => {
-        setHasFocus(false)
-    }
-
     const handleKeyBoardNavigation = (e) => {
         if(e.key === "ArrowDown"){
-            console.log("arrow down")
+            isVisible ? 
+                    setCursor(preValue => (preValue < filteredArray.length - 1 ? preValue + 1 : preValue)) :
+                    showSuggestion();
         }
         if(e.key === "ArrowUp"){
-            console.log("arrow up")
+            setCursor(preValue => (preValue > 0 ? preValue - 1 : 0))
+        }
+        if(e.key === "Escape"){
+            hideSuggestion();
+        }
+        if(e.key === "Backslash"){
+            console.log("uifieiuyiyie")
         }
     }
 
     return(
         <>
-            <div className="search-container">
-                {!hasFocus && <i class="fa fa-search search-container--icon" aria-hidden="true"></i>}
+            <div className="search-container" ref={searchContainer}>
+                {!isVisible && <i class="fa fa-search search-container--icon" aria-hidden="true"></i>}
                 <input 
                     type="text" 
                     placeholder="Search here..." 
                     className="search-container--searchbar" 
                     onChange={handleOnChange}
-                    onFocus={handleOnFocus}
-                    onBlur={handleOnBlur}
-                    onKeyDown={handleKeyBoardNavigation}
+                    onClick={showSuggestion}
+                    onKeyDown={e => handleKeyBoardNavigation(e)}
                 />
             </div>
-            {hasFocus && <div className="search-result">
+            {isVisible && <div className="search-result" ref={searchResultRef}>
                 {filteredArray.map((value,key) => {
                     return(
-                        <div className="list-group" key={key}>
+                        <div className={`list-group ${cursor === key ? "active" : ""}`} key={key} >
                             {value}
                         </div>
                     );
